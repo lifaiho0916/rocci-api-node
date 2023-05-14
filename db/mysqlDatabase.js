2fbef9c1f6267ea6f06b1ae47a4614066d780657
Nicolò Risitano <nicolo@genomeup.com>
2023-05-14 17:52:21 +0200

    modificato DB

--- db/mysqlDatabase.js
+++ db/mysqlDatabase.js
@@ -2,9 +2,9 @@ const mysql = require("mysql2");
 
 var connection = mysql.createPool({
   user: 'root',
-  host: 'localhost',
+  host: 'srv-captain--roccidb-db',
   database: 'rocci',
-  // password: '',
+  password: '23yGqcMk1@ye',
   // ssl: { fake_flag_to_enable_tls: true },
 });
 
